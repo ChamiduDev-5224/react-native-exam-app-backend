a25ca81bb4463afa0e3cce7e99a7eddb0872289c
Chamidu-circlebook <pplchamiduravihara@gmail.com>
2022-12-04 23:21:33 +0530

    student post and find all done

--- Controller/student.controller.js
+++ Controller/student.controller.js
@@ -1,17 +1,37 @@
-const getAllStudent = (req, res) => {
-  return res.send("api work");
+const { default: mongoose } = require("mongoose");
+const stSchema = require("../Models/students.model");
+
+const getAllStudent = async (req, res) => {
+  const students = await stSchema.find();
+  res.status(200).send(students);
 };
 
-const postData = (req, res) => {
+const postData = async (req, res) => {
   if (!req.body.name) {
     return res.status(400).json({
       message: "Bad request",
     });
   }
-  return res.send(`Id is : ${req.body.name}`);
-};
+  const postStudentData = await stSchema.create({
+    indexNo: req.body.indexNo,
+    name: req.body.name,
+    email: req.body.email,
+    contactNo: req.body.contactNo,
+    birthDay: req.body.birthDay,
+  });
 
+  return res.status(200).json(postStudentData);
+};
+const getById = async (req, res) => {
+  const getByiD = await stSchema.findOne({ name: req.params.id });
+  if (!getByiD?.name)
+    return res.status(400).json({
+      message: "Bad request",
+    });
+  res.status(200).send(getByiD);
+};
 module.exports = {
   getAllStudent,
   postData,
+  getById,
 };
--- Models/students.model.js
+++ Models/students.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const studentSchema = mongoose.Schema({
   indexNo: { type: String, required: true },
-  Name: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  birthYear: { type: String, required: true },
+  contactNo: { type: String, required: true, max: 10 },
+  birthDay: { type: String, required: true },
 });
 
-module.exports = mongoose.model("stSchema", studentSchema);
+module.exports = mongoose.model("students", studentSchema);
